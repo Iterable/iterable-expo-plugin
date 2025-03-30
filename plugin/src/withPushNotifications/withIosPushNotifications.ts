1e8077b344a9cc6836b65a70b6659d4b6e871d69
Loren Posen <loren.posen@iterable.com>
2025-03-30 14:30:50 -0700

    feature: add support for background modes in iOS push notifications configuration

--- plugin/src/withPushNotifications/withIosPushNotifications.ts
+++ plugin/src/withPushNotifications/withIosPushNotifications.ts
@@ -1,7 +1,8 @@
 import {
   ConfigPlugin,
   withEntitlementsPlist,
-  withPlugins
+  withPlugins,
+  withInfoPlist,
 } from 'expo/config-plugins';
 
 import { ConfigPluginProps } from '../withIterable.types';
@@ -22,17 +23,38 @@ const withCapabilities: ConfigPlugin<ConfigPluginProps> = (config, props) => {
   });
 }
 
+const withBackgroundModes: ConfigPlugin<ConfigPluginProps> = (config, props) => {
+  return withInfoPlist(config, (newConfig) => {
+    const backgroundModes = newConfig.modResults['UIBackgroundModes'] || [];
+
+    /**
+     * Add remote-notification to the background modes if it's not already there
+     * @see Step 3.5.2 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-5-set-up-support-for-push-notifications
+     */
+    if (!backgroundModes.includes('remote-notification')) {
+      newConfig.modResults.UIBackgroundModes = [...backgroundModes, 'remote-notification'];
+    }
+
+    return newConfig;
+  });
+}
+
 /**
  * Adds a fully configured push notification service to the app unless
  * `props.autoConfigurePushNotifications` is `false`.
  */
 export const withIosPushNotifications: ConfigPlugin<ConfigPluginProps> = (config, props) => {
+  /**
+   * No need to do anything if `props.autoConfigurePushNotifications` is
+   * explicitly set to `false`.
+   */
   if (props.autoConfigurePushNotifications === false) {
     return config;
   }
   
   return withPlugins(config, [
     [withCapabilities, props],
+    [withBackgroundModes, props],
   ]);
 }
 
