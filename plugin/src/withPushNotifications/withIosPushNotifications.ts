475dade51e6144aec2b8cb3e8225f49ec958d8ec
Loren Posen <loren.posen@iterable.com>
2025-03-30 16:55:37 -0700

    feature: implement notification service file generation for iOS push notifications

--- plugin/src/withPushNotifications/withIosPushNotifications.ts
+++ plugin/src/withPushNotifications/withIosPushNotifications.ts
@@ -3,22 +3,26 @@ import {
   withEntitlementsPlist,
   withPlugins,
   withInfoPlist,
+  withDangerousMod,
 } from 'expo/config-plugins';
 
-import { ConfigPluginProps } from '../withIterable.types';
+import { ConfigPluginPropsWithDefaults } from '../withIterable.types';
+import { NS_TARGET_NAME, NS_PLIST_FILE_NAME, NS_ENTITLEMENTS_FILE_NAME, NS_MAIN_FILE_NAME, NS_FILES, NS_MAIN_FILE_CONTENT, NS_PLIST_CONTENT, NS_ENTITLEMENTS_CONTENT } from './withIosPushNotifications.constants';
+
+const fs = require('fs');
+const path = require('path');
 
 /**
  * Adds anything that would be added by going to Xcode > Signing & Capabilities
  * and clicking "+ Capability"
  */
-const withCapabilities: ConfigPlugin<ConfigPluginProps> = (config, props) => {
+const withCapabilities: ConfigPlugin<ConfigPluginPropsWithDefaults> = (config, props) => {
   return withEntitlementsPlist(config, (newConfig) => {
     /**
      * Add push notification capabilities to the app.
      * @see Step 3.5.1 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-5-set-up-support-for-push-notifications
      */
-    const appEnvironment = props?.appEnvironment || 'development';
-    newConfig.modResults['aps-environment'] = appEnvironment;
+    newConfig.modResults['aps-environment'] = props?.appEnvironment;
 
     /**
      * Add the entitlement to allow time-sensitive notifications if
@@ -33,7 +37,10 @@ const withCapabilities: ConfigPlugin<ConfigPluginProps> = (config, props) => {
   });
 }
 
-const withBackgroundModes: ConfigPlugin<ConfigPluginProps> = (config, props) => {
+/**
+ * Adds any needed background modes to the app.
+ */
+const withBackgroundModes: ConfigPlugin<ConfigPluginPropsWithDefaults> = (config, props) => {
   return withInfoPlist(config, (newConfig) => {
     const backgroundModes = newConfig.modResults['UIBackgroundModes'] || [];
 
@@ -50,10 +57,53 @@ const withBackgroundModes: ConfigPlugin<ConfigPluginProps> = (config, props) => 
 }
 
 /**
+ * Adds the notification service files to the app.
+ */
+const withNSFiles: ConfigPlugin<ConfigPluginPropsWithDefaults> = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    (newConfig) => {
+      const { projectRoot, platformProjectRoot } = newConfig.modRequest;
+      const srcPath = path.resolve(
+        projectRoot,
+        platformProjectRoot
+      );
+      
+      // create a new folder
+      const newFolderPath = path.resolve(srcPath, NS_TARGET_NAME);
+      if (!fs.existsSync(newFolderPath)) {
+        fs.mkdirSync(newFolderPath)
+      }
+      
+      // add the notification service file
+      const notificationServicePath = path.resolve(srcPath, NS_TARGET_NAME, NS_MAIN_FILE_NAME);
+      if (!fs.existsSync(notificationServicePath)) {
+        fs.writeFileSync(notificationServicePath, NS_MAIN_FILE_CONTENT);
+      }
+      
+      // add the notification plist file
+      const notificationPlistPath = path.resolve(srcPath, NS_TARGET_NAME, NS_PLIST_FILE_NAME);
+      if (!fs.existsSync(notificationPlistPath)) {
+        fs.writeFileSync(notificationPlistPath, NS_PLIST_CONTENT);
+      }
+      
+      // add the notifications entitlements file
+      const entitlementsPath = path.resolve(srcPath, NS_TARGET_NAME, NS_ENTITLEMENTS_FILE_NAME);
+      if (!fs.existsSync(entitlementsPath)) {
+        fs.writeFileSync(entitlementsPath, NS_ENTITLEMENTS_CONTENT);
+      }
+
+      return newConfig;
+    },
+  ]);
+}
+
+
+/**
  * Adds a fully configured push notification service to the app unless
  * `props.autoConfigurePushNotifications` is `false`.
  */
-export const withIosPushNotifications: ConfigPlugin<ConfigPluginProps> = (config, props) => {
+export const withIosPushNotifications: ConfigPlugin<ConfigPluginPropsWithDefaults> = (config, props) => {
   /**
    * No need to do anything if `props.autoConfigurePushNotifications` is
    * explicitly set to `false`.
@@ -65,6 +115,7 @@ export const withIosPushNotifications: ConfigPlugin<ConfigPluginProps> = (config
   return withPlugins(config, [
     [withCapabilities, props],
     [withBackgroundModes, props],
+    [withNSFiles, props],
   ]);
 }
 
