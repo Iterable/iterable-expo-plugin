1dafc90a5318d5d22841127d51125264b0d3822d
Loren Posen <loren.posen@iterable.com>
2025-03-30 12:34:08 -0700

    feat: enhance IterableAppDelegate to support push notification handling and tracking

--- ios/ExpoAdapterIterable/IterableAppDelegate.swift
+++ ios/ExpoAdapterIterable/IterableAppDelegate.swift
@@ -4,10 +4,24 @@ import UserNotifications
 import IterableSDK
 
 public class IterableAppDelegate: ExpoAppDelegateSubscriber, UIApplicationDelegate {
+
+  public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ITBInfo()
+        setupUserNotificationCenter()
+
+        return true
+    }
   
   public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      ITBInfo()
       IterableAPI.register(token: deviceToken)
-      setupUserNotificationCenter()
+      // See step 7.5: https://support.iterable.com/hc/en-us/articles/360035018152-Iterable-s-iOS-SDK#step-7-5-handle-incoming-push-notifications-and-enable-push-notification-tracking
+      UNUserNotificationCenter.current().delegate = self
+  }
+
+  public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+      ITBInfo()
+      IterableAppIntegration.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
   }
 
   public func applicationDidBecomeActive(_ application: UIApplication) {
@@ -44,5 +58,17 @@ public class IterableAppDelegate: ExpoAppDelegateSubscriber, UIApplicationDelega
               ITBInfo("Already authorized")
           }
       }
+    }
+}
+
+// Handle incoming push notifications and enable push notification tracking
+// Step 7.5: https://support.iterable.com/hc/en-us/articles/360035018152-Iterable-s-iOS-SDK#step-7-5-handle-incoming-push-notifications-and-enable-push-notification-tracking
+extension IterableAppDelegate: UNUserNotificationCenterDelegate {
+    public func userNotificationCenter(_: UNUserNotificationCenter, willPresent _: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.badge, .banner, .list, .sound])
+    }
+
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        IterableAppIntegration.userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
     }
 }
