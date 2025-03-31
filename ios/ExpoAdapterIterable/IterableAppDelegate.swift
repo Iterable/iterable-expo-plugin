2691889eb73c537cdd28d015c5eac18de0f66eff
Loren Posen <loren.posen@iterable.com>
2025-03-30 18:45:55 -0700

    feature: add support for in-app messages in IterableAppDelegate for iOS

--- ios/ExpoAdapterIterable/IterableAppDelegate.swift
+++ ios/ExpoAdapterIterable/IterableAppDelegate.swift
@@ -25,6 +25,19 @@ public class IterableAppDelegate: ExpoAppDelegateSubscriber, UIApplicationDelega
     return true
   }
 
+  /**
+    * Add support for in-app messages
+    * @see Step 3.6 of https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-3-6-add-support-for-in-app-messages
+    */
+  func application(
+    _ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    IterableAppIntegration.application(
+      application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler
+    )
+  }
+
   public func application(
     _ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
   ) {
