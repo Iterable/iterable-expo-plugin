e8e947495db91e73256a2f7158daad5b2a5ff388
Loren Posen <loren.posen@iterable.com>
2025-03-30 21:12:39 -0700

    feature: enhance Android push notifications support by adding Google Services configuration and updating build.gradle handling

---
+++ plugin/src/withPushNotifications/withAndroidPushNotifications.constants.ts
@@ -0,0 +1,3 @@
+export const GOOGLE_SERVICES_CLASS_PATH = 'com.google.gms:google-services';
+export const GOOGLE_SERVICES_PLUGIN = 'com.google.gms.google-services';
+export const GOOGLE_SERVICES_VERSION = '4.3.10';
--- plugin/src/withPushNotifications/withAndroidPushNotifications.ts
+++ plugin/src/withPushNotifications/withAndroidPushNotifications.ts
@@ -1,11 +1,83 @@
-import { ConfigPlugin } from 'expo/config-plugins';
+import {
+  ConfigPlugin,
+  withProjectBuildGradle,
+  withPlugins,
+  WarningAggregator,
+  withAppBuildGradle,
+} from 'expo/config-plugins';
 
 import { ConfigPluginPropsWithDefaults } from '../withIterable.types';
+import {
+  GOOGLE_SERVICES_CLASS_PATH,
+  GOOGLE_SERVICES_PLUGIN,
+  GOOGLE_SERVICES_VERSION,
+} from './withAndroidPushNotifications.constants';
+function setBuildscriptDependency(buildGradle: string) {
+  if (!buildGradle.includes(GOOGLE_SERVICES_CLASS_PATH)) {
+    return buildGradle.replace(
+      /dependencies\s?{/,
+      `dependencies {
+        classpath('${GOOGLE_SERVICES_CLASS_PATH}:${GOOGLE_SERVICES_VERSION}')`
+    );
+  } else {
+    return buildGradle;
+  }
+}
+
+function applyPlugin(appBuildGradle: string) {
+  // Make sure the project does not have the plugin already
+  const pattern = new RegExp(
+    `apply\\s+plugin:\\s+['"]${GOOGLE_SERVICES_PLUGIN}['"]`
+  );
+  if (!appBuildGradle.match(pattern)) {
+    return appBuildGradle + `\napply plugin: '${GOOGLE_SERVICES_PLUGIN}'`;
+  }
+
+  return appBuildGradle;
+}
+
+/**
+ * Add the Google Services dependencies to the project and build.gradle file if
+ * they don't exist.
+ * @see Step 4.1 https://support.iterable.com/hc/en-us/articles/360045714132-Installing-Iterable-s-React-Native-SDK#step-4-1-set-up-firebase
+ */
+const withFirebase: ConfigPlugin<ConfigPluginPropsWithDefaults> = (config) => {
+  config = withProjectBuildGradle(config, async (newConfig) => {
+    if (newConfig.modResults.language === 'groovy') {
+      newConfig.modResults.contents = setBuildscriptDependency(
+        newConfig.modResults.contents
+      );
+    } else {
+      WarningAggregator.addWarningAndroid(
+        '@iterable/expo-plugin',
+        `Cannot automatically configure project build.gradle if it's not groovy`
+      );
+    }
+
+    return newConfig;
+  });
+
+  config = withAppBuildGradle(config, (newConfig) => {
+    if (newConfig.modResults.language === 'groovy') {
+      newConfig.modResults.contents = applyPlugin(
+        newConfig.modResults.contents
+      );
+    } else {
+      WarningAggregator.addWarningAndroid(
+        '@iterable/expo-plugin',
+        `Cannot automatically configure app build.gradle if it's not groovy`
+      );
+    }
+    return newConfig;
+  });
+
+  return config;
+};
 
 export const withAndroidPushNotifications: ConfigPlugin<
   ConfigPluginPropsWithDefaults
 > = (config, props) => {
-  return config;
+  return withPlugins(config, [[withFirebase, props]]);
 };
 
 export default withAndroidPushNotifications;
