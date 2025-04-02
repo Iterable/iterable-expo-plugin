3d7f9ccf3ed5f1b9abbd9d607bbc53828c490a32
Loren Posen <loren.posen@iterable.com>
2025-04-02 13:13:47 -0700

    Add utility functions for managing Android Gradle dependencies and plugins
    - Introduced `addAndroidProjectDependency`, `addAndroidAppDependency`, and `addAndroidApplyPlugin` functions in `utils.ts` to streamline the addition of dependencies and plugins in Android build.gradle files.
    - Updated `withAndroidPushNotifications.ts` to utilize the new utility functions, improving code organization and maintainability.
    - Removed redundant dependency management code from `withAndroidPushNotifications.ts`.

--- plugin/src/withPushNotifications/withAndroidPushNotifications.ts
+++ plugin/src/withPushNotifications/withAndroidPushNotifications.ts
@@ -20,87 +20,17 @@ import {
 
 import path from 'path';
 import fs from 'fs';
-
-interface GradleDependency {
-  classpath: string;
-  version?: string;
-}
-
-/**
- * Add a dependency to the project build.gradle file.
- */
-function addProjectDependency(buildGradle: string, options: GradleDependency) {
-  if (!buildGradle.includes(options?.classpath)) {
-    return buildGradle.replace(
-      /dependencies\s?{/,
-      `dependencies {
-        classpath('${options?.classpath}${
-        options?.version ? `:${options?.version}` : ''
-      }')`
-    );
-  } else {
-    return buildGradle;
-  }
-}
-
-interface AppGradleDependency extends GradleDependency {
-  /**
-   * The string to add to the dependencies block.
-   *
-   * If this is not provided, ${classpath}:${version} will be used.
-   */
-  implementation?: string;
-}
-
-/**
- * Add a dependency to the app build.gradle file.
- */
-function addAppDependency(buildGradle: string, options: AppGradleDependency) {
-  if (!buildGradle.includes(options?.classpath)) {
-    const implementationString =
-      options?.implementation ??
-      `'${options?.classpath}${
-        options?.version ? `:${options?.version}` : ''
-      }'`;
-    return buildGradle.replace(
-      /dependencies\s?{/,
-      // NOTE: awkard spacing is intentional -- it ensure correct alignment in
-      // the output build.gradle file
-      `dependencies {
-    implementation ${implementationString}`
-    );
-  } else {
-    return buildGradle;
-  }
-}
-
-/**
- * Add the apply plugin line to the app build.gradle file if it doesn't exist.
- */
-function addApplyPlugin(appBuildGradle: string, pluginName: string) {
-  // Check for `apply plugin: 'com.google.gms.google-services'`
-  const applyPluginPattern = new RegExp(
-    `apply\\s+plugin:\\s+['"]${pluginName}['"]`
-  );
-  // Check for `plugins { id 'com.google.gms.google-services' }`
-  const pluginIdPattern = new RegExp(`id\\s+['"]${pluginName}['"]`);
-
-  // Make sure the project does not have the plugin already
-  if (
-    !appBuildGradle.match(applyPluginPattern) &&
-    !appBuildGradle.match(pluginIdPattern)
-  ) {
-    return appBuildGradle + `\napply plugin: '${pluginName}'`;
-  }
-
-  return appBuildGradle;
-}
+import {
+  addAndroidAppDependency,
+  addAndroidProjectDependency,
+  addAndroidApplyPlugin,
+} from '../utils';
 
 const withFirebaseInProjectBuildGradle: ConfigPlugin<ConfigPluginPropsWithDefaults> =
   (config) => {
     return withProjectBuildGradle(config, async (newConfig) => {
       if (newConfig.modResults.language === 'groovy') {
-        newConfig.modResults.contents = addProjectDependency(
+        newConfig.modResults.contents = addAndroidProjectDependency(
           newConfig.modResults.contents,
           {
             classpath: GOOGLE_SERVICES_CLASS_PATH,
@@ -122,17 +52,17 @@ const withFirebaseInAppBuildGradle: ConfigPlugin<ConfigPluginPropsWithDefaults> 
   (config) => {
     return withAppBuildGradle(config, (newConfig) => {
       if (newConfig.modResults.language === 'groovy') {
-        newConfig.modResults.contents = addApplyPlugin(
+        newConfig.modResults.contents = addAndroidApplyPlugin(
           newConfig.modResults.contents,
           GOOGLE_SERVICES_PLUGIN
         );
-        newConfig.modResults.contents = addAppDependency(
+        newConfig.modResults.contents = addAndroidAppDependency(
           newConfig.modResults.contents,
           {
             classpath: FIREBASE_MESSAGING_CLASS_PATH,
           }
         );
-        newConfig.modResults.contents = addAppDependency(
+        newConfig.modResults.contents = addAndroidAppDependency(
           newConfig.modResults.contents,
           {
             classpath: FIREBASE_BOM_CLASS_PATH,
