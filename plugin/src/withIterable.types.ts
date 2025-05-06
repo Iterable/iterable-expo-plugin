135171a8d11c7f8c7681668e3a60be5f2b675524
Loren Posen <loren.posen@iterable.com>
2025-05-06 13:03:48 -0700

    chore: remove enableInAppMessages option from configuration and related types

--- plugin/src/withIterable.types.ts
+++ plugin/src/withIterable.types.ts
@@ -10,24 +10,31 @@ export interface ConfigPluginProps {
   appEnvironment?: 'development' | 'production';
   /**
    * Whether to automatically configure push notifications.
+   *
+   * If true, this will set up the necessary permissions and configurations for
+   * push notifications according to the Iterable SDK documentation.  If you
+   * would like to build your own push notification configuration, set this to
+   * false -- but do so with caution as Iterable cannot guarantee compatibility
+   * with custom push notification configurations.
    * @default true
    */
   autoConfigurePushNotifications?: boolean;
   /**
    * Whether to enable time-sensitive push notifications.
+   *
+   * (iOS only)
+   *
    * @default true
    */
   enableTimeSensitivePush?: boolean;
   /**
    * Whether to request permissions for push notifications.
+   *
+   * (iOS only)
+   *
    * @default false
    */
   requestPermissionsForPushNotifications?: boolean;
-  /**
-   * Whether to enable in-app messages.
-   * @default true
-   */
-  enableInAppMessages?: boolean;
 }
 
 export type ConfigPluginPropsWithDefaults = Required<ConfigPluginProps>;
