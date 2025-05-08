3a2cac7d470af326cb4d93568d3c33e952407aea
Loren Posen <loren.posen@iterable.com>
2025-05-07 20:55:06 -0700

    test: refactor withIosPushNotifications tests to streamline imports and enhance mock configuration usage

--- plugin/__tests__/withIosPushNotifications.test.ts
+++ plugin/__tests__/withIosPushNotifications.test.ts
@@ -1,31 +1,25 @@
-import { ExpoConfig } from 'expo/config';
-import {
-  ExportedConfigWithProps,
-  Mod,
-  ModPlatform,
-  ModProps,
-} from 'expo/config-plugins';
+import { Mod } from 'expo/config-plugins';
 import fs from 'fs';
 import path from 'path';
 import { XcodeProject } from 'xcode';
 
 import {
-  createTestConfig,
-  type WithIterableResult,
+  createMockEntitlementsConfig,
+  createMockIosDangerousModConfig,
   createMockPlistConfig,
   createMockPodfileConfig,
   createMockXcodeConfig,
-  createMockIosDangerousModConfig,
-  createMockEntitlementsConfig,
+  createTestConfig,
+  type WithIterableResult,
 } from '../__mocks__/testUtils';
 import withIterable from '../src/withIterable';
 import type { ConfigPluginProps } from '../src/withIterable.types';
 import {
   NS_ENTITLEMENTS_FILE_NAME,
   NS_MAIN_FILE_NAME,
+  NS_PLIST_FILE_NAME,
   NS_POD,
   NS_TARGET_NAME,
-  NS_PLIST_FILE_NAME,
 } from '../src/withPushNotifications/withIosPushNotifications.constants';
 
 jest.mock('fs', () => ({
