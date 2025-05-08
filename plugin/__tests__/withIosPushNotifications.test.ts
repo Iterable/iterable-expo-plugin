fef8fb171c416420268b29e23f4a15a939a990d4
Loren Posen <loren.posen@iterable.com>
2025-05-07 20:35:25 -0700

    test: add unit tests for withIosPushNotifications to validate time-sensitive push entitlement behavior

---
+++ plugin/__tests__/withIosPushNotifications.test.ts
@@ -0,0 +1,72 @@
+import { ExpoConfig } from 'expo/config';
+import {
+  ExportedConfigWithProps,
+  Mod,
+  ModPlatform,
+  ModProps,
+} from 'expo/config-plugins';
+import fs from 'fs';
+import path from 'path';
+import { XcodeProject } from 'xcode';
+
+import {
+  createTestConfig,
+  type WithIterableResult,
+  createMockPlistConfig,
+  createMockPodfileConfig,
+  createMockXcodeConfig,
+  createMockIosDangerousModConfig,
+  createMockEntitlementsConfig,
+} from '../__mocks__/testUtils';
+import withIterable from '../src/withIterable';
+import type { ConfigPluginProps } from '../src/withIterable.types';
+import {
+  NS_ENTITLEMENTS_FILE_NAME,
+  NS_MAIN_FILE_NAME,
+  NS_POD,
+  NS_TARGET_NAME,
+  NS_PLIST_FILE_NAME,
+} from '../src/withPushNotifications/withIosPushNotifications.constants';
+
+describe('withIosPushNotifications', () => {
+  describe('enableTimeSensitivePush', () => {
+    it('should add time sensitive push to the entitlements if not explicitly set to false', async () => {
+      const result = withIterable(createTestConfig(), {}) as WithIterableResult;
+      const modifiedEntitlements = await result.mods.ios.entitlements(
+        createMockEntitlementsConfig()
+      );
+      expect(
+        modifiedEntitlements.modResults[
+          'com.apple.developer.usernotifications.time-sensitive'
+        ]
+      ).toBe(true);
+    });
+
+    it('should not add time sensitive push to the entitlements if explicitly set to false', async () => {
+      const config = createTestConfig();
+      const props: ConfigPluginProps = {
+        enableTimeSensitivePush: false,
+      };
+      const result = withIterable(config, props) as WithIterableResult;
+      const modifiedEntitlements = await result.mods.ios.entitlements(
+        createMockEntitlementsConfig()
+      );
+      expect(
+        modifiedEntitlements.modResults[
+          'com.apple.developer.usernotifications.time-sensitive'
+        ]
+      ).not.toBeDefined();
+    });
+
+    it('should not add time sensitive push to the entitlements if `autoConfigurePushNotifications` is false', async () => {
+      const config = createTestConfig();
+      const props: ConfigPluginProps = {
+        enableTimeSensitivePush: true,
+        autoConfigurePushNotifications: false,
+      };
+      const result = withIterable(config, props) as WithIterableResult;
+      // The below is not defined as entitlements are not added anywhere
+      expect(result.mods?.ios?.entitlements).not.toBeDefined();
+    });
+  });
+});
